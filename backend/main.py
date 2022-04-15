aad42ccd810bca81c78432a783d957939dff63de
yenbo <yenbo@yenbode-MacBook-Pro.local>
2022-04-15 11:02:16 +0800

    haha

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,23 @@ from tabnanny import check
 from urllib import response
 from fastapi import FastAPI, HTTPException
 
-from model import Message, User, UserOut
-from database import (create_user, delete_user, find_all_user, find_message_by_author, find_message_by_author_title, password_check, post_a_message, find_all_message, user_exist, update_Message)
+from model import Message, User, UserOut, MessageOut
+from database import (erase_message_by_title, create_user, delete_user,find_message_by_key ,erase_message_by_author, find_all_user, find_message_by_author, find_message_by_author_title, password_check, post_a_message, find_all_message, user_exist, update_Message)
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:3000",
+]
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 async def root():
@@ -52,6 +64,13 @@ async def list_message_by_author(author: str):
         return response
     raise HTTPException(404, f"There is no message by the author {author}")
 
+@app.get('/key')
+async def list_message_having_key(key: str):
+    response = await find_message_by_key(key)
+    if response:
+        return response
+    raise HTTPException(404, f"There is no message by the key {key}")
+
 @app.put('/message/update')
 async def update_message(author: str, password: str, title: str, desc: str):
     d1 = await user_exist(author)
@@ -65,6 +84,21 @@ async def update_message(author: str, password: str, title: str, desc: str):
             raise HTTPException(404, f"There is no message title {title} by the author {author}")
         raise HTTPException(404, f"Wrong password!!!")
     raise HTTPException(404, f"There is no author {author}")
+
+@app.delete('/message/delete/author')
+async def delete_message_by_author(author: str):
+    response = await erase_message_by_author(author)
+    if response:
+        return "Successfully deleted messages"
+    raise HTTPException(404, f"There is no message by author {author}")
+
+@app.delete('/message/delete/title')
+async def delete_message_by_title(title: str):
+    response = await erase_message_by_title(title)
+    if response:
+        return "Successfully deleted messages"
+    raise HTTPException(404, f"There is no message title {title}")
+
     
 
 
--- backend/model.py
+++ backend/model.py
@@ -1,4 +1,6 @@
+from pickle import NONE
 from pydantic import BaseModel
+from typing import Optional
 
 class User(BaseModel):
     username: str
@@ -11,3 +13,10 @@ class Message(BaseModel):
     author: str
     topic: str
     content: str
+    key: Optional[list] = None
+
+class MessageOut(BaseModel):
+    author: str
+    topic: str
+    content: str
+
