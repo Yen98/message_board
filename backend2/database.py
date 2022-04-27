b75d34dc642457f3ad3774fb044a82c3994844c7
yenbo <yenbo@yenbode-MacBook-Pro.local>
2022-04-27 20:22:37 +0800

    online

--- backend2/database.py
+++ backend2/database.py
@@ -1,8 +1,9 @@
 from model import Room, Message
 import motor.motor_asyncio
 import re
+from bson.objectid import ObjectId
 
-client = motor.motor_asyncio.AsyncIOMotorClient('mongodb://localhost:27017/')
+client = motor.motor_asyncio.AsyncIOMotorClient('mongodb+srv://yenbo98:yenbo98@cluster0.1od1a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
 database = client.Chat_Room
 room_collection = database.room
 message_collection = database.message
@@ -20,10 +21,8 @@ async def get_a_room_info(room_name):
 
 async def write_a_message(message):
     document = message
-    document["tag"] = re.split(',[ ]*|[ ]+|\.[ ]*|\(|\)|[!?;][ ]*', document["content"])
     result = await message_collection.insert_one(document)
-    result2 = await room_collection.update_one({"name": document["room_name"]}, {"$inc": {"number_of_messages": 1}})
-    result3 = await room_collection.update_one({"name": document["room_name"]}, {"$push": {"conversation": result.inserted_id}})
+    result2 = await room_collection.update_one({"name": document["room_name"]}, {"$inc": {"number_of_messages": 1}, "$push": {"conversation": result.inserted_id}})
     return message
 
 async def find_message(room_name, begin, end):
@@ -31,22 +30,26 @@ async def find_message(room_name, begin, end):
     room = await room_collection.find_one({"name": room_name})
     for i in range(begin, end):
         document = await message_collection.find_one({"_id": room["conversation"][i]}, {"_id": 0})
-        document.pop("tag")
         messages.append(document)
     return messages
 
 async def find_message_with_tag(room_name, tag):
     messages = []
-    # result = message_collection.find({"room_name": room_name, "tag": tag}, {"_id": 0}).limit(50)
-    result = message_collection.find( { "$text": { "$search": tag }}, {"_id": 0, "tag": 0} ).limit(50)
+    result = message_collection.find( { "$text": { "$search": tag }}, {"_id": 0} ).limit(50)
     async for document in result:
         messages.append(document)
     return messages
 
+async def find_message_with_id(id):
+    document = await message_collection.find_one({"_id": ObjectId(id)}, {"_id": 0})
+    return document
 
-    
-    
-    
-        
-    
-    
+async def delete_message(id):
+    result = await room_collection.update_one({"conversation": ObjectId(id)}, {"$inc": {"number_of_messages": -1}, "$pull": {"conversation": ObjectId(id)}})
+    await message_collection.delete_one({"_id": ObjectId(id)})
+    return True
+
+async def delete_room(name):
+    await room_collection.delete_one({"name": name})
+    await message_collection.delete_many({"room_name": name})
+    return True
--- backend2/main.py
+++ backend2/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from database import create_a_room, get_a_room_info, write_a_message, find_message, find_message_with_tag
+from database import create_a_room, get_a_room_info, write_a_message, find_message, find_message_with_tag, find_message_with_id, delete_message, delete_room
 
 from model import Room, Message, Messages
 
@@ -28,9 +28,14 @@ async def get_recent_message(room_name: str):
     response = await find_message(room_name, 11671, 11681)
     return response
 
-@app.get('/message/tag/{room_name}')
-async def serach_message(room_name: str, tag: str):
-    response = await find_message_with_tag(room_name, tag)
+@app.get('/message/keyword/{room_name}')
+async def serach_message(room_name: str, keyword: str):
+    response = await find_message_with_tag(room_name, keyword)
+    return response
+
+@app.get('/find/{id}')
+async def get_message_by_id(id: str):
+    response = await find_message_with_id(id)
     return response
 
 @app.post('/room', response_model=Room)
@@ -54,4 +59,18 @@ async def create_many_message(message: Messages):
     if response:
         return {"haha"}
     return HTTPException(400, "Something went wrong")
+
+@app.delete('/delete/message/{id}')
+async def erase_message(id: str):
+    response = await delete_message(id)
+    if response:
+        return "Successfully deleted message"
+    raise HTTPException(404, f"There is no message with id {id}")
+
+@app.delete('/delete/room/{name}}')
+async def close_room(name: str):
+    response = await delete_room(name)
+    if response:
+        return "Successfully deleted room"
+    raise HTTPException(404, f"There is no room name {name}")
 
